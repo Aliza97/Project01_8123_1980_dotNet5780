901d889759830cf1b570edb8ef8bb2ee46421d7d
afitouss <afitouss@T-NOFECH07>
2020-01-23 11:22:08 +0200

    new pages

---
+++ PLWPF/איזור _אישי3.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PLWPF
+{
+    /// <summary>
+    /// Interaction logic for איזור__אישי3.xaml
+    /// </summary>
+    public partial class איזור__אישי3 : Page
+    {
+        public איזור__אישי3()
+        {
+            InitializeComponent();
+        }
+    }
+}
